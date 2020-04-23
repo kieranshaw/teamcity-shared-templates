62e811630c0cf7a0fe9ff35e4e5076c3f4c75eab
Kieran Shaw <kieran@kieranshaw.co.uk>
2020-04-23 10:40:36 +0100

    Fixed imports

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.buildReportTab
 
 /*
